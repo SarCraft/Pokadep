29258707c0d2cbf9c81fad0cad18a92c59f21d4d
SarCraft <nathan.henaux30@gmail.com>
2024-06-17 19:57:31 +0200

    Dock Mobile Device

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LightsComponent } from './components/lights/lights.component';
+import { DockComponent } from './components/dock/dock.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, LightsComponent],
+  imports: [RouterOutlet, NavbarComponent, LightsComponent, DockComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
