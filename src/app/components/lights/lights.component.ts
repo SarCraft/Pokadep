b4ebed42e2fe1bbf596ea25ada90556c0cad7071
SarCraft <nathan.henaux30@gmail.com>
2024-06-17 17:05:45 +0200

    Lights

---
+++ src/app/components/lights/lights.component.ts
@@ -0,0 +1,11 @@
+import { Component, AfterViewInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lights',
+  standalone: true,
+  templateUrl: './lights.component.html',
+  styleUrls: ['./lights.component.css']
+})
+export class LightsComponent {
+
+}
