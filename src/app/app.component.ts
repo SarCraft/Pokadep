4b489a1e0ba4ef020a7f95e1ae137d10464bdcc3
SarCraft <nathan.henaux30@gmail.com>
2024-06-21 09:19:12 +0200

    Add mini card to website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LightsComponent } from './components/lights/lights.component';
 import { DockComponent } from './components/dock/dock.component';
+import { MinimizeCardComponent } from './components/minimize-card/minimize-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, LightsComponent, DockComponent],
+  imports: [RouterOutlet, NavbarComponent, LightsComponent, DockComponent, MinimizeCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
