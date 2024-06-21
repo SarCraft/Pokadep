b573e77085cf29792effb9782c31b4efe925bd72
SarCraft <nathan.henaux30@gmail.com>
2024-06-21 19:22:48 +0200

    Add new components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,21 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LightsComponent } from './components/lights/lights.component';
 import { DockComponent } from './components/dock/dock.component';
-import { MinimizeCardComponent } from './components/minimize-card/minimize-card.component';
+import { StaticCarouselComponent } from './components/static-carousel/static-carousel.component';
+import { ScrollIconComponent } from './components/scroll-icon/scroll-icon.component';
+import { DetailedCardComponent } from './components/detailed-card/detailed-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, LightsComponent, DockComponent, MinimizeCardComponent],
+  imports: [RouterOutlet,
+            NavbarComponent, 
+            LightsComponent, 
+            DockComponent, 
+            StaticCarouselComponent,
+            ScrollIconComponent,
+            DetailedCardComponent,
+          ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
