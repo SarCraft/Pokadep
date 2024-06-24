806407f9d7732bed562829c495a85ec68bf162a0
SarCraft <nathan.henaux30@gmail.com>
2024-06-24 09:05:58 +0200

    Add components at website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,21 +6,34 @@ import { DockComponent } from './components/dock/dock.component';
 import { StaticCarouselComponent } from './components/static-carousel/static-carousel.component';
 import { ScrollIconComponent } from './components/scroll-icon/scroll-icon.component';
 import { DetailedCardComponent } from './components/detailed-card/detailed-card.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { PokeapiService } from './service/pokeapi.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,
+  imports: [
+            RouterOutlet,
             NavbarComponent, 
             LightsComponent, 
             DockComponent, 
             StaticCarouselComponent,
             ScrollIconComponent,
             DetailedCardComponent,
+            FooterComponent,
+            SearchBarComponent,
           ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [PokeapiService],
 })
 export class AppComponent {
-  title = 'app';
+  pokemon: any;
+
+  constructor(private pokeapiService: PokeapiService) {}
+
+  updatePokemon(pokemon: any) {
+    this.pokemon = pokemon;
+  }
 }
